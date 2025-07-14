dd9b6e2cef73c72acd3dc84390043ef87ec40247
weihuang <thoughtworks.ht.huang@mbusa.com>
2025-07-14 21:53:45 +0800

    homework

--- src/main/java/com/bootcamp/springBootDemo/Employee.java
+++ src/main/java/com/bootcamp/springBootDemo/Employee.java
@@ -8,7 +8,10 @@ public class Employee {
     private Gender gender;
     private Double salary;
 
-    public Employee(int id, String name, Integer age, Gender gender, Double salary) {
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, Integer age, Gender gender, Double salary) {
         this.id = id;
         this.name = name;
         this.age = age;
